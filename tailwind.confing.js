42a9e7c2fddea30bb341fe9a1f8569f1fcf2bad6
vinaysolanki535 <vinaysolanki535@gmail.com>
2021-12-01 19:43:15 +0530

    frontend part ui in vue almosted completed

---
+++ tailwind.confing.js
@@ -0,0 +1,29 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false,
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        blue: '#1DA1F2',
+        darkblue: '#2795D9',
+        lightblue: '#EFF9FF',
+        dark: '#657786',
+        light: '#AAB8C2',
+        lighter: '#E1E8ED',
+        lightest: '#F5F8FA',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
